ad53ab8fe8af38062005d0d611dfdc9cbec7b045
Titouan <dk.titouan@gmail.com>
2024-06-28 02:07:11 +0200

    import parser

--- __tests__/parser-4-1-2.test.ts
+++ __tests__/parser-4-1-2.test.ts
@@ -1,5 +1,5 @@
 import { beforeEach, describe, expect, test } from "@jest/globals";
-import { createParser } from "../src/parser";
+import { createParser, parseRgaaSpreadsheet } from "../src/parser";
 
 const nbCriteriaPerPage = 106;
 
